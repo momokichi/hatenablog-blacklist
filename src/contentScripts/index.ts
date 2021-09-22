bae70c5de2cf49223c8cf5482865641d77be340f
Suzuki Kensuke <s1811349@s.tsukuba.ac.jp>
2021-09-22 21:51:52 +0900

    set blacklist from popup

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -1,16 +1,11 @@
 /* eslint-disable no-console */
-import { onMessage } from 'webext-bridge'
 
 // Firefox `browser.tabs.executeScript()` requires scripts return a primitive value
-(() => {
+(async () => {
   console.info('[vitesse-webext] Hello world from content script')
 
-  // communication example: send previous tab title from background page
-  onMessage('tab-prev', ({ data }) => {
-    console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
-  })
-
-  const blackList = ['https://example.hatenablog.com/*']
+  const data = await browser.storage.local.get('blackList')
+  const blackList: string[] = data.blackList.split('\n')
 
   const htmlCollection = document.getElementsByClassName('serviceTop-entry-recommend')
   const targets = Array.from(htmlCollection)
--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -2,22 +2,36 @@
   <main class="w-[300px] px-4 py-5 text-center text-gray-700">
     <Logo />
     <div>Popup</div>
-    <p class="mt-2 opacity-50">
-      This is the popup page
-    </p>
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-    <div class="mt-2">
-      <span class="opacity-50">Storage:</span> {{ storageDemo }}
+    <div class="w-full">
+      <textarea v-model="blackList" class="border border-gray-400 rounded w-full py-1 mt-2" />
     </div>
+    <div>
+      {{ blackList.split('\n') }}
+    </div>
+
+    <button class="btn mt-2" @click="setValue()">
+      set
+    </button>
   </main>
 </template>
 
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+/* eslint-disable no-console */
+const blackList = ref('')
 
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
+const loadBlackList = async() => {
+  try {
+    const storage = await browser.storage.local.get('blackList')
+    blackList.value = storage.blackList
+  }
+  catch (e) {
+    console.error(e)
+  }
 }
+loadBlackList()
+
+function setValue() {
+  browser.storage.local.set({ blackList: blackList.value })
+}
+
 </script>
